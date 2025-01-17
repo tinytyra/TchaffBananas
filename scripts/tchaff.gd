b9929cb26ac09d5309411465977fd2efde6c2306
tinytyra <tyraravn@gmail.com>
2025-01-17 20:23:49 +0100

    trouble hooting some fucky animations

--- scripts/tchaff.gd
+++ scripts/tchaff.gd
@@ -9,6 +9,38 @@ var screen_size # Size of the game window
 
 func _ready():
 	screen_size = get_viewport_rect().size
+	$AnimationPlayer.play("RESET")
+	$AnimatedSprite2D.animation = "wobble_down"
+	$AnimatedSprite2D.play()
+
+func _on_body_entered(_body: Node2D) -> void:
+	hp -= 1
+	hit.emit(hp)
+	$DeathMihh.play()
+	$AnimatedSprite2D.pause()
+	$AnimationPlayer.play("HitAnimation")
+	if hp == 0:
+		died.emit()
+		$AnimationPlayer.queue("DeathAnimation")
+	else:
+		$AnimationPlayer.queue("RESET")
+
+# Gives the player 1.5s of invulnerability after being hit
+func _on_hit(_hp) -> void:
+	$CollisionShape2D.set_deferred("disabled", true)
+	if hp > 0:
+		await get_tree().create_timer(1.5).timeout
+		$CollisionShape2D.disabled = false
+		$AnimatedSprite2D.play()
+
+func start(pos):
+	position = pos
+	hp = 3
+	$AnimationPlayer.queue("RESET")
+	$CollisionShape2D.disabled = false
+	# reset animation states and reenable hitbox
+	$AnimatedSprite2D.animation = "wobble_down"
+	$AnimatedSprite2D.play()
 
 func _physics_process(delta):
 	var velocity = Vector2.ZERO
@@ -46,23 +78,3 @@ func _physics_process(delta):
 				$AnimatedSprite2D.rotation_degrees = +45 # rotate sprite down right
 			elif velocity.y < 0: # if also moving up
 				$AnimatedSprite2D.rotation_degrees = -45 # rotate sprite up right
-
-func _on_body_entered(_body: Node2D) -> void:
-	hit.emit(hp)
-	$DeathMihh.play()
-	$AnimationPlayer.play("HitAnimation")
-	hp -= 1
-	await get_tree().create_timer(1.0).timeout
-	if hp == 0: # if ur ded
-		died.emit()
-		hide()
-	else:
-		$AnimationPlayer.play("RESET")
-
-func start(pos):
-	position = pos
-	show()
-	hp = 3
-	$CollisionShape2D.disabled = false
-	$AnimatedSprite2D.animation = "wobble_down" # reset to down animation
-	$AnimatedSprite2D.play()
